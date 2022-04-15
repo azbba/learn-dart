61b8217d16c8c8a0783cb9935f500625ca87325c
azbba <az34bba@gmail.com>
2022-04-15 13:34:52 +0200

    Dart Strings

---
+++ lessonFiles/009-strings.dart
@@ -0,0 +1,50 @@
+void main(List<String> args) {
+  /**
+   * Dart Strings:
+   * A string variable is used to hold series or sequence of characters – letters, numbers, and special characters.
+   * ============================
+   * Printing String:
+   * the [ print() ] function is used to print the formatted message to the screen, or other standard output device
+   * ============================
+   * String Concatenation:
+   * In Dart, strings can be concatenated simply using the plus [ + ] operator or the [ += ] assignment operator.
+   * ===========================
+   * String Interpolation
+   * String interpolation is the process of evaluating a string containing placeholders, variables and interpolated expressions. 
+   * When an interpolated string is evaluated the placeholders, variables and expressions are replaced with their corresponding values. 
+   * In Dart, ${expression} is used for string interpolation.
+   * Exp:
+   * int age = 30;
+   * print( "my after 10 years is ${ age + 10 }" );
+   * ===========================
+   * String Properties:
+   * [01] =>  String.length       => The length of the string
+   * [02] =>  String.isEmpty      => Whether this string is empty.
+   * [03] =>  String.isNotEmpty   => Whether this string is not empty.
+   * [04] =>  String.codeUnits    => An unmodifiable list of the UTF-16 code units of this string.
+   * [05] =>  String.hashCode     => A hash code derived from the code units of the string
+   * [06] =>  String.runes        => An Iterable of Unicode code-points of this string
+   * [07] =>  String.runtimeType  => A representation of the runtime type of the object.
+   * ===========================
+   * String Methods
+   * [0] =>  String.allMatches(String, StartIndex)                      => Matches this pattern against the string repeatedly
+   * [0] =>  String.codeUnitAt(index)                                   => Returns the 16-bit UTF-16 code unit at the given index
+   * [0] =>  String.compareTo(otherString)                              => Compares this string to other [ Returns values are = 0 when equl, 1 when string 1 greater than string 2, -1 when string 1 less than string 2 ]
+   * [0] =>  String.contains(Pattern, StartIndex)                       => Whether this string contains a match of other.
+   * [0] =>  String.endsWith(other)                                     => Whether this string ends with other
+   * [0] =>  String.indexOf(Pattern, StartIndex)                        => Returns the position of the first match of pattern in this string, starting at
+   * [0] =>  String.lastIndexOf(Pattern, StartIndex)                    => The starting position of the last match pattern in this string
+   * [0] =>  String.replaceAll(Pattern, replace)                        => Replaces all substrings that match pattern with replace
+   * [0] =>  String.replaceFirst(Pattern, replace, Pattern, replace)    => Creates a new string with the first occurrence of from replaced by to.
+   * [0] =>  String.replaceRange(start, end, replacement)               => Replaces the substring from start to end with replacement.
+   * [0] =>  String.split(pattern)                                      => Splits the string at matches of pattern and returns a list of substrings
+   * [0] =>  String.startsWith(Pattern, StartIndex)                     => Whether this string starts with a match of pattern
+   * [0] =>  String.substring(start, end)                               => The substring of this string from start, inclusive, to end
+   * [0] =>  String.toLowerCase()                                       => Converts all characters in this string to lower case
+   * [0] =>  String.toString()                                          => A string representation of this object
+   * [0] =>  String.toUpperCase()                                       => Converts all characters in this string to upper case
+   * [0] =>  String.trim()                                              => The string without any leading and trailing whitespace.
+   * [0] =>  String.trimLeft()                                          => The string without any leading whitespace
+   * [0] =>  String.trimRight()                                         => The string without any trailing whitespace
+  */
+}
